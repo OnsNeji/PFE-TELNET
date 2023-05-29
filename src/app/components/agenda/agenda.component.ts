8f4c3546f5d6aee1113a20784ea336759eb4cc4a
OnsNeji <onsneji9@gmail.com>
2023-05-29 01:04:50 +0100

    agenda

--- src/app/components/agenda/agenda.component.ts
+++ src/app/components/agenda/agenda.component.ts
@@ -7,11 +7,13 @@ import { Utilisateur } from 'app/models/shared/utilisateur.model.js';
 import { Departement } from 'app/models/shared/departement.model.js';
 import { MatDialog } from '@angular/material/dialog';
 import { UserCardComponent } from '../user-card/user-card.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-agenda',
   templateUrl: './agenda.component.html',
-  styleUrls: ['./agenda.component.scss']
+  styleUrls: ['./agenda.component.scss'],
 })
 export class AgendaComponent implements OnInit {
 
@@ -20,6 +22,8 @@ export class AgendaComponent implements OnInit {
   departements!: Departement[];
   searchTerm: string;
   employees: Utilisateur[];
+  id: number;
+  private jwtHelper = new JwtHelperService();
 
   constructor(private route: ActivatedRoute, private Service: ApiService, public dialog: MatDialog) { }
 
@@ -30,6 +34,12 @@ export class AgendaComponent implements OnInit {
       const id = params['id'];
       this.getSiteById(id);
     });
+    const token = localStorage.getItem('token');
+    if (token) {
+      const decodedToken = this.jwtHelper.decodeToken(token);
+      this.id = decodedToken.nameid;  
+      console.log(this.id);
+    }
   }
 
 
